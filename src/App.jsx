4cd96f1a27a230bfad5362868a94c1f3235d483e
Rustam Guseinov <rg20031991.com>
2024-02-29 19:10:56 +0300

    rm unless component commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import { ProductsPage } from "./pages/ProductsPage";
 import { HomePage } from "./pages/HomePage";
 import { NotFoundPage } from "./pages/NotFoundPage";
-import { Blog } from "./pages/Blog";
 import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
@@ -11,12 +10,10 @@ function App() {
       <header>
         <Link to="/">Home</Link>
         <Link to="/product">ProductsPage</Link>
-        <Link to="/posts">Blog</Link>
       </header>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/product" element={<ProductsPage />} />
-        <Route path="/posts" element={<Blog />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
