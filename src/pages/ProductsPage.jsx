4af2892cf75991dfe9b8ca50b33044cb9fb3960f
Rustam Guseinov <rg20031991.com>
2024-02-29 16:06:54 +0300

    we repeat the answer if the error has been fixed

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import md5 from "md5";
-
 export const ProductsPage = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [products, setProducts] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(() => {
+    return parseInt(localStorage.getItem("currentPage")) || 1;
+  });
   const [productsPerPage] = useState(50);
 
   useEffect(() => {
@@ -15,9 +16,9 @@ export const ProductsPage = () => {
         const timestamp = new Date()
           .toISOString()
           .split("T")[0]
-          .replace(/-/g, ""); // таймштамп
+          .replace(/-/g, "");
         const password = "Valantis"; // пароль
-        const authString = md5(`${password}_${timestamp}`); // инициируем строку авторизации
+        const authString = md5(`${password}_${timestamp}`);
 
         const response = await axios.post(
           "http://api.valantis.store:40000/",
@@ -30,7 +31,7 @@ export const ProductsPage = () => {
           },
           {
             headers: {
-              "X-Auth": authString, // передача строки в заголовке
+              "X-Auth": authString, // Строка в заголовке
             },
           }
         );
@@ -39,7 +40,6 @@ export const ProductsPage = () => {
           throw new Error("Failed to fetch product IDs");
         }
 
-        // Удаление повторяющихся productIds как в тз
         const productIds = [...new Set(response.data.result)];
 
         if (productIds.length === 0) {
@@ -48,7 +48,7 @@ export const ProductsPage = () => {
           return;
         }
 
-        // Второй запрос для получения информации
+        // Второй запрос о товарах
         const secondResponse = await axios.post(
           "http://api.valantis.store:40000/",
           {
@@ -72,15 +72,25 @@ export const ProductsPage = () => {
         setProducts(detailedProducts);
         setLoading(false);
       } catch (error) {
-        setError(error.message);
-        setLoading(false);
+        // При ошибки в ответе API
+        if (
+          error.response &&
+          error.response.data &&
+          error.response.data.error
+        ) {
+          console.error("API Error:", error.response.data.error);
+          // Повторяем запрос
+          fetchProducts();
+        } else {
+          setError(error.message);
+          setLoading(false);
+        }
       }
     };
 
     fetchProducts();
   }, [currentPage]);
 
-  // Расчет индексов
   const indexOfLastProduct = currentPage * productsPerPage;
   const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
   const currentProducts = products.slice(
@@ -88,15 +98,24 @@ export const ProductsPage = () => {
     indexOfLastProduct
   );
 
+  //пагинция
   const goToPrevPage = () => {
-    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+    if (currentPage > 1) {
+      setCurrentPage((prevPage) => prevPage - 1);
+    }
   };
 
+  // пагинция
   const goToNextPage = () => {
-    setCurrentPage((prevPage) =>
-      Math.min(prevPage + 1, Math.ceil(products.length / productsPerPage))
-    );
+    if (indexOfLastProduct < products.length) {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
   };
+
+  useEffect(() => {
+    //  localStorage
+    localStorage.setItem("currentPage", currentPage.toString());
+  }, [currentPage]);
 
   if (loading) {
     return <div>Loading...</div>;
@@ -119,16 +138,14 @@ export const ProductsPage = () => {
           </li>
         ))}
       </ol>
-   
+      {/* пагинация */}
       <div>
         <button onClick={goToPrevPage} disabled={currentPage === 1}>
           Предыдущая страница
         </button>
         <button
           onClick={goToNextPage}
-          disabled={
-            currentPage === Math.ceil(products.length / productsPerPage)
-          }
+          disabled={indexOfLastProduct >= products.length}
         >
           Следующая страница
         </button>
