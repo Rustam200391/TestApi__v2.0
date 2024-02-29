b20c29e69eae7dcf8709b9b1d3b76850cd45aabe
Rustam Guseinov <rg20031991.com>
2024-02-29 15:33:21 +0300

    add pagination

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import md5 from "md5"; // Импортируем библиотеку для вычисления MD5
+import md5 from "md5";
 
 export const ProductsPage = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [products, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [productsPerPage] = useState(50);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -13,23 +15,22 @@ export const ProductsPage = () => {
         const timestamp = new Date()
           .toISOString()
           .split("T")[0]
-          .replace(/-/g, ""); // Генерация таймштампа
+          .replace(/-/g, ""); // таймштамп
         const password = "Valantis"; // пароль
-        const authString = md5(`${password}_${timestamp}`); // Формирование строки авторизации
+        const authString = md5(`${password}_${timestamp}`); // инициируем строку авторизации
 
-        // Первый запрос для получения списка идентификаторов товаров
         const response = await axios.post(
           "http://api.valantis.store:40000/",
           {
             action: "get_ids",
             params: {
-              offset: 0,
-              limit: 10,
+              offset: (currentPage - 1) * productsPerPage,
+              limit: productsPerPage,
             },
           },
           {
             headers: {
-              "X-Auth": authString, // Передача авторизационной строки в заголовке
+              "X-Auth": authString, // передача строки в заголовке
             },
           }
         );
@@ -38,7 +39,7 @@ export const ProductsPage = () => {
           throw new Error("Failed to fetch product IDs");
         }
 
-        // Удаление повторяющихся productIds
+        // Удаление повторяющихся productIds как в тз
         const productIds = [...new Set(response.data.result)];
 
         if (productIds.length === 0) {
@@ -47,7 +48,7 @@ export const ProductsPage = () => {
           return;
         }
 
-        // Второй запрос для получения информации о товарах
+        // Второй запрос для получения информации
         const secondResponse = await axios.post(
           "http://api.valantis.store:40000/",
           {
@@ -66,7 +67,6 @@ export const ProductsPage = () => {
         }
 
         const detailedProducts = secondResponse.data.result;
-        console.log(secondResponse.data.result);
 
         // Обновление products
         setProducts(detailedProducts);
@@ -78,7 +78,25 @@ export const ProductsPage = () => {
     };
 
     fetchProducts();
-  }, []);
+  }, [currentPage]);
+
+  // Расчет индексов
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = products.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
+
+  const goToPrevPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
+
+  const goToNextPage = () => {
+    setCurrentPage((prevPage) =>
+      Math.min(prevPage + 1, Math.ceil(products.length / productsPerPage))
+    );
+  };
 
   if (loading) {
     return <div>Loading...</div>;
@@ -90,16 +108,31 @@ export const ProductsPage = () => {
 
   return (
     <div>
-      <h1>Products</h1>
-      <ul>
-        {products.map((product, index) => (
+      <h1>Products:</h1>
+      <ol>
+        {currentProducts.map((product, index) => (
           <li key={`${product.id}_${index}`}>
-            <h2>{product.product}</h2>
+            {/* отрисовываем с проверкой, если нуль то не выводим */}
+            {product?.product && <h2>{product.product}</h2>}
             {product?.brand && <p>Brand: {product.brand}</p>}
-            <p>Price:{product.price}</p>
+            {product?.price && <p>Price: {product.price}</p>}
           </li>
         ))}
-      </ul>
+      </ol>
+   
+      <div>
+        <button onClick={goToPrevPage} disabled={currentPage === 1}>
+          Предыдущая страница
+        </button>
+        <button
+          onClick={goToNextPage}
+          disabled={
+            currentPage === Math.ceil(products.length / productsPerPage)
+          }
+        >
+          Следующая страница
+        </button>
+      </div>
     </div>
   );
 };
